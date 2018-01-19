d60f6462ac27d40f4ffd721d16341ce6b73e91bc
asifur2018 <tanima@infosolz.com>
2018-01-19 11:27:38 +0530

    commit except Large File

---
+++ InvoicePOS/InvoicePOS/UserControll/Invoice/InvoiceList.xaml.cs
@@ -0,0 +1,40 @@
+﻿using InvoicePOS.Models;
+using InvoicePOS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InvoicePOS.UserControll.Invoice
+{
+    /// <summary>
+    /// Interaction logic for Invoice.xaml
+    /// </summary>
+    public partial class Invoice : UserControl
+    {
+        InvoiceViewModel _InvoiceViewModel;
+        public Invoice()
+        {
+            InitializeComponent();
+            _InvoiceViewModel = new InvoiceViewModel();
+            this.DataContext = _InvoiceViewModel;
+        }
+
+        private void grdInvoice_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var str = (GetInvoiceModel)dataGrid1.SelectedItem;
+            _InvoiceViewModel.View_INvoice();
+        }
+
+    }
+
+}
---
+++ InvoicePOS/InvoicePOS/UserControll/StockTransfer/StockList.xaml.cs
@@ -0,0 +1,39 @@
+﻿using InvoicePOS.UserControll.Item;
+using InvoicePOS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InvoicePOS.UserControll.StockTransfer
+{
+    /// <summary>
+    /// Interaction logic for StockList.xaml
+    /// </summary>
+    public partial class StockList : UserControl
+    {
+        ManageStockViewModel _ManageStockViewModel;
+        public StockList()
+        {
+            InitializeComponent();
+            _ManageStockViewModel = new ManageStockViewModel();
+            this.DataContext = _ManageStockViewModel;
+        }
+
+       
+        
+
+
+
+    }
+}
